d29d85d893dee84a864bb48008f2694d37659b10
Fred Blundun <fred@snowplowanalytics.com>
2014-09-11 09:22:34 +0100

    Added test for tracker setter methods using multiple subjects

--- Snowplow.Tracker/Snowplow.Tracker.Tests/SubjectTest.cs
+++ Snowplow.Tracker/Snowplow.Tracker.Tests/SubjectTest.cs
@@ -46,6 +46,7 @@ namespace Snowplow.Tracker.Tests
             var expected = new Dictionary<string, string>
             {
                 {"p", "mob"},
+                {"uid", "malcolm"},
                 {"res", "100x200"},
                 {"vp", "50x60"},
                 {"cd", "24"},
@@ -55,6 +56,49 @@ namespace Snowplow.Tracker.Tests
             foreach (string key in expected.Keys)
             {
                 Assert.AreEqual(subject.nvPairs[key], expected[key]);
+            }
+        }
+
+        [TestMethod]
+        public void testTrackerSetterMethods()
+        {
+            var subjectOne = new Subject();
+            var subjectTwo = new Subject();
+            var tracker = new Tracker(new Emitter("localhost"), subjectOne);
+            tracker.setPlatform("mob");
+            tracker.setUserId("malcolm");
+            tracker.setScreenResolution(100, 200);
+            tracker.setViewport(50, 60);
+            tracker.setColorDepth(24);
+            tracker.setTimezone("Europe London");
+            tracker.setLang("en");
+            tracker.setSubject(subjectTwo);
+            tracker.setUserId("6561");
+            tracker.setLang("fr");
+            tracker.setScreenResolution(150, 250);
+            var expectedOne = new Dictionary<string, string>
+            {
+                {"p", "mob"},
+                {"uid", "malcolm"},
+                {"res", "100x200"},
+                {"vp", "50x60"},
+                {"cd", "24"},
+                {"tz", "Europe London"},
+                {"lang", "en"},
+            };
+            var expectedTwo = new Dictionary<string, string>
+            {
+                {"p", "pc"},
+                {"res", "150x250"},
+                {"lang", "fr"},
+            };
+            foreach (string key in expectedOne.Keys)
+            {
+                Assert.AreEqual(subjectOne.nvPairs[key], expectedOne[key]);
+            }
+            foreach (string key in expectedTwo.Keys)
+            {
+                Assert.AreEqual(subjectTwo.nvPairs[key], expectedTwo[key]);
             }
         }
     }
