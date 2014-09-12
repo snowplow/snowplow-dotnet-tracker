2d4dbd8ff637148611f870c287cc258b14c2043d
Fred Blundun <fred@snowplowanalytics.com>
2014-09-12 12:33:06 +0100

    Added RedisEmitter class

---
+++ Snowplow.Tracker/RedisEmitter.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * RedisEmitter.cs
+ *
+ * Copyright (c) 2014 Snowplow Analytics Ltd. All rights reserved.
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License
+ * Version 2.0. You may obtain a copy of the Apache License Version 2.0 at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the Apache License Version 2.0 for the specific
+ * language governing permissions and limitations there under.
+ * Authors: Fred Blundun
+ * Copyright: Copyright (c) 2014 Snowplow Analytics Ltd
+ * License: Apache License Version 2.0
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sider;
+using System.Web.Script.Serialization;
+
+namespace Snowplow.Tracker
+{
+    public class RedisEmitter : IEmitter
+    {
+        private RedisClient rdb;
+        private String key;
+
+        public RedisEmitter(RedisClient rdb = null, string key = "snowplow")
+        {
+            this.rdb = rdb ?? new RedisClient();
+            this.key = key;
+        }
+
+        public void Input(Dictionary<string, string> payload)
+        {
+            rdb.RPush(key, new JavaScriptSerializer(null).Serialize(payload));
+        }
+
+        public void Flush(bool sync = false)
+        {
+
+        }
+
+    }
+}
