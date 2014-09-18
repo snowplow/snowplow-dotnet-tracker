cee32acd952103da243e3a14c58c681e557c97a5
Fred Blundun <fred@snowplowanalytics.com>
2014-09-18 16:41:50 +0100

    Created Events directory for TransactionItem and Platform

---
+++ Snowplow.Tracker/Events/TransactionItem.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * TransactionItem.cs
+ * 
+ * Copyright (c) 2014 Snowplow Analytics Ltd. All rights reserved.
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License
+ * Version 2.0. You may obtain a copy of the Apache License Version 2.0 at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the Apache License Version 2.0 for the specific
+ * language governing permissions and limitations there under.
+ * Authors: Fred Blundun
+ * Copyright: Copyright (c) 2014 Snowplow Analytics Ltd
+ * License: Apache License Version 2.0
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowplow.Tracker
+{
+    public struct TransactionItem
+    {
+        public string sku;
+        public double price;
+        public int quantity;
+        public string name;
+        public string category;
+
+        public TransactionItem(string sku, double price, int quantity, string name = null, string category = null)
+        {
+            this.sku = sku;
+            this.price = price;
+            this.quantity = quantity;
+            this.name = name;
+            this.category = category;
+        }
+    }
+}
