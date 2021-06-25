e4e70c1f92b6967f25ebdb2ff1704fa9943b832e
Paul Boocock <mail@paulboocock.net>
2021-06-25 10:48:06 +0100

    Update Copyright to 2021 (close #108)

--- Snowplow.Tracker.PlatformExtensions/Snowplow.Tracker.PlatformExtensions.iOS/Properties/AssemblyInfo.cs
+++ Snowplow.Tracker.PlatformExtensions/Snowplow.Tracker.PlatformExtensions.iOS/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Snowplow Analytics LTD")]
 [assembly: AssemblyProduct("Snowplow.Tracker.PlatformExtensions.iOS")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
